58adf3b5cb3d6f77e40c7b4f0480a7a9fd658488
PerLycke <per.lycke@gmail.com>
2015-04-06 10:19:58 +0200

    compile kernel with a15 optimized Linaro GCC 4.9 2015.03
    Change-Id: I844923eff4f8dca6d95bf9a5e4e1f71c5d695d11

--- envsetup.sh
+++ envsetup.sh
@@ -181,7 +181,7 @@ function setpaths()
     case $ARCH in
         arm)
             # Legacy toolchain configuration used for ARM kernel compilation
-            toolchaindir=arm/arm-cortex-linux-gnueabi-linaro_4.9.3-2015.03/bin
+            toolchaindir=arm/arm-cortex_a15-linux-gnueabihf-linaro_4.9.3-2015.03/bin
             if [ -d "$gccprebuiltdir/$toolchaindir" ]; then
                  export ARM_EABI_TOOLCHAIN="$gccprebuiltdir/$toolchaindir"
                  ANDROID_KERNEL_TOOLCHAIN_PATH="$ARM_EABI_TOOLCHAIN":
